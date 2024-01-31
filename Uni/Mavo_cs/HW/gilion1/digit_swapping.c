7a6e4c8a4447aec7eec63a00bcdda0960ef6f6dc
Omri Rozenblat <omrirozenb@gmail.com>
2024-01-31 21:17:45 +0200

    updated_HW

--- Uni/Mavo_cs/HW/gilion1/digit_swapping.c
+++ Uni/Mavo_cs/HW/gilion1/digit_swapping.c
@@ -1,65 +1,63 @@
-//#include <stdio.h>
-//
-//int main() {
-//
-//    int full_number;
-//    int first_position;
-//    int second_position;
-//    printf("Please enter the number: ");
-//    scanf("%d", &full_number);
-//    printf("Please enter the positions: ");
-//    scanf("%d%d",&first_position, &second_position);
-//
-//    int number_first_pos;
-//    int number_second_pos;
-//
-//    // if position is one just do modulu
-//    if (first_position == 1) {
-//        number_first_pos = (full_number % 10);
-//    }
-//    else {
-//        // keep dividing until reaching the position and then modulu
-//        int counter = 1;
-//        //init a temp number just to find the digit
-//        int temp_full_number = full_number;
-//        // iterate until found the number in position
-//        while(counter < first_position){
-//            temp_full_number = temp_full_number / 10;
-//            counter++;
-//        }
-//        number_first_pos = temp_full_number % 10;
-//    }
-//
-//    if (second_position == 1) {
-//        number_second_pos = (full_number % 10);
-//    }
-//    else {
-//        int counter = 1;
-//        int temp_full_number = full_number;
-//        // iterate until found the number in position
-//        while (counter < second_position) {
-//            temp_full_number = temp_full_number / 10;
-//            counter++;
-//        }
-//        number_second_pos = temp_full_number % 10;
-//    }
-//    //find the delta between the 2 numbers
-//    int delta = number_first_pos-number_second_pos;
-//
-//    //how much we need to factor the delta
-//    int first_pos_delta_factor = 1;
-//    for (int i=0; i < second_position-1; i++){
-//        first_pos_delta_factor*=10;
-//    }
-//
-//    int second_pos_delta_factor = 1;
-//    for (int i=0; i < first_position-1; i++){
-//        second_pos_delta_factor *= 10;
-//    }
-//
-//    //add and subtract the delta in the relevant place
-//    full_number = full_number + delta * first_pos_delta_factor - delta * second_pos_delta_factor;
-//    printf("The new number is %d", full_number);
-//
-//    return 0;
-//}
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    int first_position;
+    int second_position;
+    printf("Please enter the number:\n");
+    scanf("%d", &num);
+    printf("Please enter the positions:\n");
+    scanf("%d%d",&first_position, &second_position);
+
+    int number_first_pos;
+    int number_second_pos;
+
+    // if position is one just do modulo
+    if (first_position == 1) {
+        number_first_pos = (num % 10);
+    } else {
+        // keep dividing until reaching the position and then modulo
+        int counter = 1;
+        //init a temp number just to find the digit
+        int temp_full_number = num;
+        // iterate until found the number in position
+        while(counter < first_position){
+            temp_full_number = temp_full_number / 10;
+            counter++;
+        }
+        number_first_pos = temp_full_number % 10;
+    }
+
+    if (second_position == 1) {
+        number_second_pos = (num % 10);
+    } else {
+        int counter = 1;
+        int temp_full_number = num;
+        // iterate until found the number in position
+        while (counter < second_position) {
+            temp_full_number = temp_full_number / 10;
+            counter++;
+        }
+        number_second_pos = temp_full_number % 10;
+    }
+    //find the delta between the 2 numbers
+    int delta = number_first_pos-number_second_pos;
+
+    //how much we need to factor the delta
+    int first_pos_delta_factor = 1;
+    for (int i=0; i < second_position-1; i++){
+        first_pos_delta_factor*=10;
+    }
+
+    int second_pos_delta_factor = 1;
+    for (int i=0; i < first_position-1; i++){
+        second_pos_delta_factor *= 10;
+    }
+
+    //add and subtract the delta in the relevant place
+    num += delta * first_pos_delta_factor - delta * second_pos_delta_factor;
+    printf("The new number is: %d", num);
+
+    return 0;
+}
--- Uni/Mavo_cs/HW/gilion1/noise_cleaning.c
+++ Uni/Mavo_cs/HW/gilion1/noise_cleaning.c
@@ -2,17 +2,57 @@
 //
 //int main() {
 //
-//    int k;
-//    int m;
 //    int num;
-//    scanf("%d", &num);
-//    if (num % 5 == 0) {\\\
-//        k = 5;
-//    }
-//    else {
-//        k = 3;
-//    }
+//    int k;
+//    int num_counter = 0;
+//    int sum_to_avg_5;
+//    int sum_to_avg_3;
+//    int num_1;
+//    int num_2;
+//    int num_3;
+//    int num_4;
+//    int num_5;
+//    printf("Please Enter Input Signal:\n");
+//    printf("The Clean Signal Is: ");
 //
-//    for (int i=0; i < k; i++ );
+//    while (scanf("%d", &num) != EOF) {
+//        //print space and comma after each number
+//        if (num_counter > 0) {
+//            printf(", ");
+//        }
+//        num_5 = num_4;
+//        num_4 = num_3;
+//        num_3 = num_2;
+//        num_2 = num_1;
+//        num_1 = num;
+//        sum_to_avg_5 = num_1 + num_2 + num_3 + num_4 + num_5;
+//        sum_to_avg_3 = num_1 + num_2 + num_3;
+//
+//        //decide value of k
+//        if (num % 5 == 0){
+//            k = 5;
+//        } else {
+//            k = 3;
+//        }
+//
+//        //print original value if not able to avg
+//        if (num_counter < 2) {
+//            printf("%.2f", (float)num);
+//
+//        } else {
+//            if (num % 5 == 0){
+//                if (num_counter > 3) {
+//                    printf("%.2f", (float)sum_to_avg_5 / k);
+//                } else {
+//                    printf("%.2f", (float)num);
+//                }
+//
+//            } else {
+//                printf("%.2f", (float)sum_to_avg_3 / k);
+//            }
+//        }
+//        num_counter++;
+//
+//    }
 //    return 0;
 //}
