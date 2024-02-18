119b40f6669997643a68e6b453ccd3e2a33c87ea
Omri Rozenblat <omrirozenb@gmail.com>
2024-02-18 17:19:10 +0200

    finishedpallindromalmost

--- Uni/Mavo_cs/HW/gilion2/pelindrom.c
+++ Uni/Mavo_cs/HW/gilion2/pelindrom.c
@@ -1,20 +1,56 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool is_pellindrom(int);
+bool is_palindrome(int);
 int calc_num_length(int num);
+bool calc_num_sum_even(int num);
+
 
 int main() {
     printf("Please enter a number:\n");
 
 //    scanf(" %u", &num);
 //    printf("%u", num);
-    int num = 456;
-    bool is = is_pellindrom(num);
+    int num = 64577546;
+    bool is = is_palindrome(num);
+    int res = is;
+    if (is == true){
+        bool is_even = calc_num_sum_even(num);
+        res += is_even;
+        return res;
+    }
+    printf("%d", res);
+    return  res;
 
-
-    return 0;
 }
+
+
+bool is_palindrome(int num) {
+
+    int num_len = calc_num_length(num);
+    int pelindrom_nums_array[num_len];
+    int temp_num = num;
+
+        //add pellindrom numbers to array
+    for (int i = 0; i<num_len;i++){
+        pelindrom_nums_array[i] = temp_num % 10;
+        temp_num = temp_num / 10;
+    }
+
+    int start_counter = 0;
+    int end_counter = num_len-1;
+    // iterate thrugh the array and see if its pellindrom
+    while (start_counter <= end_counter) {
+        if (pelindrom_nums_array[start_counter] == pelindrom_nums_array[end_counter]){
+            start_counter += 1;
+            end_counter -= 1;
+            continue;
+        } else {
+            return  false;
+        }
+    }
+    return true;
+    }
 
 int calc_num_length(int num) {
     //count digits
@@ -29,17 +65,17 @@ int calc_num_length(int num) {
     }
     return num_length;
 }
-
-bool is_pellindrom(int num) {
-
-    int num_len = calc_num_length(num);
-    int pelindrom_nums_array[num_len];
+bool calc_num_sum_even(int num){
     int temp_num = num;
-    for (int i = 0; i<num_len;i++){
-
-        pelindrom_nums_array[i] = temp_num % 10;
+    int sum = 0;
+    while (temp_num > 9) {
+        sum += temp_num % 10;
         temp_num = temp_num / 10;
     }
-    return true;
+    sum += temp_num % 10;
+    if (sum % 2 == 0){
+        return  true;
 
     }
+    return false;
+}
