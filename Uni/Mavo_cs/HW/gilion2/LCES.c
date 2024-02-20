9cc4207f3c7a2a5fbce9bf0c21f456d9bcb7dab0
Omri Rozenblat <omrirozenb@gmail.com>
2024-02-20 20:46:15 +0200

    finished3

--- Uni/Mavo_cs/HW/gilion2/LCES.c
+++ Uni/Mavo_cs/HW/gilion2/LCES.c
@@ -2,25 +2,78 @@
 #include <stdlib.h>
 
 void longestContinuousEvenSequence(int arr[], int len);
+void copy_arr_to_arr(int arr_1[], int arr_2[], int len);
+void print_arr_items(int arr[], int len);
 
 int main() {
-    int len_array = 0;
-    // First enter the size of the array
-    scanf("%d", &len_array);
-    // allocating the array - will be explained later in the course
-    int* arr = malloc(sizeof(int) * len_array);
-    if(arr) {
-        // Enter the numbers that the array will hold
-        for (int start_index = 0; start_index < len_array; start_index++) {
-            scanf("%d", &(arr[start_index]));
-        }
-        longestContinuousEvenSequence(arr, len_array);
-    }
-    // Freeing the array - will be explained later in the course
-    free(arr);
+//    int len_array = 0;
+//    // First enter the size of the array
+//    scanf("%d", &len_array);
+//    // allocating the array - will be explained later in the course
+//    int* arr = malloc(sizeof(int) * len_array);
+//    if(arr) {
+//        // Enter the numbers that the array will hold
+//        for (int start_index = 0; start_index < len_array; start_index++) {
+//            scanf("%d", &(arr[start_index]));
+//        }
+//        longestContinuousEvenSequence(arr, len_array);
+//    }
+//    // Freeing the array - will be explained later in the course
+//    free(arr);
+
+//    int arr[10] = {2, 4,6,6,4,11,14,16,18,23};
+//    int arr[10] = {2, 4,6,8,4,11,14,16,18,23};
+    int arr[10] = {0, 2, 4, 8, 10, 12};
+//    int arr[10] = {1,1,1};
+
+
+    longestContinuousEvenSequence(arr, 6);
     return 0;
 }
 
+
 void longestContinuousEvenSequence(int arr[], int len){
-    
+    int seq_arr[len], temp_arr[len];
+    int main_seq_counter = 0;
+    int second_pointer = 1;
+    for (int i =0; i < len; i++){
+        if (arr[i] % 2 == 0){
+            while (arr[i+second_pointer] - arr[i+second_pointer-1] == 2){
+                if (second_pointer == 1){
+                    temp_arr[0] = arr[i];
+                }
+                temp_arr[second_pointer] = arr[i+second_pointer];
+                second_pointer += 1;
+            }
+            //if the array is bigger, run-over the current sequence array
+            if (second_pointer > main_seq_counter){
+                copy_arr_to_arr(seq_arr, temp_arr, len);
+                //skip to the next item after the sequence
+                i = i + second_pointer;
+                //count the sequence length
+                main_seq_counter = second_pointer;
+                second_pointer = 1;
+            }
+        }
+    }
+    //prints
+    if (main_seq_counter == 0){
+        printf("No even sequence in the array");
+    } else{
+        print_arr_items(seq_arr, main_seq_counter);
+    }
+
+}
+
+void copy_arr_to_arr(int arr_1[], int arr_2[], int len){
+    for (int j = 0; j< len; j++){
+        arr_1[j] = arr_2[j];
+    }
+}
+
+void print_arr_items(int arr[], int len){
+    printf("The longest sequence with continuous even numbers is: ");
+    for (int i=0; i< len; i++){
+        printf("%d ", arr[i]);
+    }
 }
--- Uni/Mavo_cs/HW/gilion2/pelindrom.c
+++ Uni/Mavo_cs/HW/gilion2/pelindrom.c
@@ -1,85 +1,85 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-bool is_palindrome(int);
-int calc_num_length(int num);
-bool calc_num_sum_even(int num);
-int isDoublePalindrome(int num);
-
-int main(){
-    int num;
-    printf("Please enter a number:\n");
-    if(scanf("%d", &num) != 1){
-        return 0;
-    }
-    if(isDoublePalindrome(num) == 1){
-        printf("The number %d is palindrome", num);
-    }else if (isDoublePalindrome(num) == 2){
-        printf("The number %d is double palindrome", num);
-    }else{
-        printf("The number is not palindrome");
-    }
-}
-
-int isDoublePalindrome(int num){
-    bool is = is_palindrome(num);
-    int res = is;
-    if (is == true){
-        bool is_even = calc_num_sum_even(num);
-        res += is_even;
-        return res;
-    }
-    return  res;
-}
-
-bool is_palindrome(int num) {
-
-    int num_len = calc_num_length(num);
-    int palindrome_nums_array[num_len];
-    int temp_num = num;
-
-    //add palindrome numbers to array
-    for (int i = 0; i<num_len;i++) {
-        palindrome_nums_array[i] = temp_num % 10;
-        temp_num = temp_num / 10;
-    }
-
-    int start_counter = 0;
-    int end_counter = num_len-1;
-    // iterate thrugh the array and see if its pellindrom
-    while (start_counter <= end_counter) {
-        if (palindrome_nums_array[start_counter] == palindrome_nums_array[end_counter]){
-            start_counter += 1;
-            end_counter -= 1;
-        } else {
-            return  false;
-        }
-    }
-    return true;
-    }
-
-int calc_num_length(int num) {
-    //count digits
-    int num_length = 1;
-    int temp_num = num;
-
-    while (temp_num > 9) {
-        num_length += 1;
-        temp_num = temp_num / 10;
-    }
-    return num_length;
-}
-bool calc_num_sum_even(int num){
-    int temp_num = num;
-    int sum = 0;
-    while (temp_num > 9) {
-        sum += temp_num % 10;
-        temp_num = temp_num / 10;
-    }
-    sum += temp_num % 10;
-    if (sum % 2 == 0){
-        return  true;
-
-    }
-    return false;
-}
+//#include <stdio.h>
+//#include <stdbool.h>
+//
+//bool is_palindrome(int);
+//int calc_num_length(int num);
+//bool calc_num_sum_even(int num);
+//int isDoublePalindrome(int num);
+//
+//int main(){
+//    int num;
+//    printf("Please enter a number:\n");
+//    if(scanf("%d", &num) != 1){
+//        return 0;
+//    }
+//    if(isDoublePalindrome(num) == 1){
+//        printf("The number %d is palindrome", num);
+//    }else if (isDoublePalindrome(num) == 2){
+//        printf("The number %d is double palindrome", num);
+//    }else{
+//        printf("The number is not palindrome");
+//    }
+//}
+//
+//int isDoublePalindrome(int num){
+//    bool is = is_palindrome(num);
+//    int res = is;
+//    if (is == true){
+//        bool is_even = calc_num_sum_even(num);
+//        res += is_even;
+//        return res;
+//    }
+//    return  res;
+//}
+//
+//bool is_palindrome(int num) {
+//
+//    int num_len = calc_num_length(num);
+//    int palindrome_nums_array[num_len];
+//    int temp_num = num;
+//
+//    //add palindrome numbers to array
+//    for (int i = 0; i<num_len;i++) {
+//        palindrome_nums_array[i] = temp_num % 10;
+//        temp_num = temp_num / 10;
+//    }
+//
+//    int start_counter = 0;
+//    int end_counter = num_len-1;
+//    // iterate thrugh the array and see if its pellindrom
+//    while (start_counter <= end_counter) {
+//        if (palindrome_nums_array[start_counter] == palindrome_nums_array[end_counter]){
+//            start_counter += 1;
+//            end_counter -= 1;
+//        } else {
+//            return  false;
+//        }
+//    }
+//    return true;
+//    }
+//
+//int calc_num_length(int num) {
+//    //count digits
+//    int num_length = 1;
+//    int temp_num = num;
+//
+//    while (temp_num > 9) {
+//        num_length += 1;
+//        temp_num = temp_num / 10;
+//    }
+//    return num_length;
+//}
+//bool calc_num_sum_even(int num){
+//    int temp_num = num;
+//    int sum = 0;
+//    while (temp_num > 9) {
+//        sum += temp_num % 10;
+//        temp_num = temp_num / 10;
+//    }
+//    sum += temp_num % 10;
+//    if (sum % 2 == 0){
+//        return  true;
+//
+//    }
+//    return false;
+//}
